c9f292f6ef19d425eecda89f8c49f6236b709d3a
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-07-02 11:19:34 +0200

    outsource natural_sort to own file and actually add it to repository

---
+++ conf_utils.py
@@ -0,0 +1,6 @@
+# Function sorting by digits independent from length
+def natural_sort(l): 
+  convert = lambda text: int(text) if text.isdigit() else text.lower() 
+  alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+  return sorted(l, key = alphanum_key)
+
